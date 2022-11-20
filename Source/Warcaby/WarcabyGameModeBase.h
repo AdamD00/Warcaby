8ba0a1c4a2a5ae6159c03573d3550014fc0c947c
Mrokon <mrokon00@gmail.com>
2022-11-20 17:44:58 +0100

    Stworzenie projektu i planszy

---
+++ Source/Warcaby/WarcabyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarcabyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARCABY_API AWarcabyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
